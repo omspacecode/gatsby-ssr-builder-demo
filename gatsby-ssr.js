e1b9b22a0ab0b80b62e856c6d7623939fa9cbb78
Om Prakash Raj Purohit <om@Oms-MacBook-Pro.local>
2023-08-17 12:54:27 +0530

    install fetch

--- gatsby-ssr.js
+++
@@ -1,4 +0,0 @@
-import * as React from "react"
-import { BuilderComponent, builder } from '@builder.io/react'
-
-builder.init(process.env.BUILDER_PUBLIC_KEY);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,41 @@
 import * as React from "react"
+import { BuilderComponent, builder } from '@builder.io/react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+builder.init(process.env.BUILDER_PUBLIC_KEY);
+
+const Page = ({ serverData }) => {
+    return (
+        <div>
+            <div>Hello world!</div>
+            <BuilderComponent model="page" content={serverData.page} /> {/*  Replace with your Builder.io model name */}
+        </div>
+    )
 }
+
+export async function getServerData() {
+    try {
+        const page =
+            (await builder
+                .get('page', {
+                    userAttributes: {
+                        urlPath: '/',
+                    },
+                })
+                .toPromise()) || null
+
+        return {
+            props: {
+                page,
+            },
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return {
+            props: {
+                // Provide an empty object or handle the error as needed
+                page: {}
+            },
+        }
+    }
+}
+export default Page
